40f7b12a13654266d6d2a9e24f0207f44c538f57
AugustHao <tianxiao.hao@telethonkids.org.au>
2026-06-24 22:42:18 +0000

    changed saved rds file path to be relative to project not machine

--- R/sub_functions.R
+++ R/sub_functions.R
@@ -118,8 +118,8 @@ rehydrate_lifehistory_function <- function(path_to_object) {
 }
 
 
-aquatic_stage <- "C:/Users/JOhiolei/OneDrive - The Kids Research Institute Australia/Documents/An-stephensi-IBM_Proj/R/das_temp_dens_As.RDS"
-adult_stage <- "C:/Users/JOhiolei/OneDrive - The Kids Research Institute Australia/Documents/An-stephensi-IBM_Proj/R/ds_temp_humid.RDS"
+aquatic_stage <- "R/das_temp_dens_As.RDS"
+adult_stage <- "R/ds_temp_humid.RDS"
 
 das_temp_dens_As <- rehydrate_lifehistory_function(aquatic_stage)
 ds_temp_humid_As <- rehydrate_lifehistory_function(adult_stage)
